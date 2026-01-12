69a2723738a488d2d034cbb16b966e09afc211fb
Jayzo-yt <jayyanth606@gmail.com>
2026-01-12 18:18:05 +0000

    added moods

--- app/(main)/journal/write/page.jsx
+++ app/(main)/journal/write/page.jsx
@@ -3,12 +3,14 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import 'react-quill-new/dist/quill.snow.css';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { journalSchema } from '@/app/lib/schema';
 import { BarLoader } from 'react-spinners';
 import { Input } from "@/components/ui/input"
 import { isLoading } from 'process';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { MOODS } from '@/app/lib/moods';
 
 const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
 
@@ -26,7 +28,7 @@ const JournalEntryPage = () => {
 
   return (
     <div className='py-8'>
-      <form>
+      <form className='space-y-2 mx-auto'>
         <h1 className="text-5xl md:text-6xl font-bold">Journal entry</h1>
         {isLoading && <BarLoader color="black" width="100%" />}
 
@@ -35,16 +37,31 @@ const JournalEntryPage = () => {
           <Input {...register('title')} placeholder="Enter the title" className={'py-5 md:text-md ${errors.title ? "border-red-500" : ""}'} />
         </div>
 
-        <div>
-          <label>What you gonna write about today?</label>
-          <ReactQuill
-            theme="snow"
-            className='mt-2 h-96'
-            {...register('content')}
-            placeholder="Write your journal entry here..."
-          />
+        <div className='space-y-2'>
+          <label>Your mood today?</label>
+          <Controller
+            name="mood"
+            control={control}
+            render={({ field }) => {
+              return (
+                <Select {...field}>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select a mood" />
+                  </SelectTrigger>
+                  <SelectContent>
+              {Object.values(MOODS).map((mood) => {
+                return (
+                  <SelectItem key={mood.id} value={mood.id}>
+                    <span className="flex items-center gap-2">
+                      {mood.emoji} {mood.label}
+                    </span>
+                  </SelectItem>
+                );
+              })}
+            </SelectContent>
+          </Select>
+              )}}  />
         </div>
-
       </form>
     </div>
   );
